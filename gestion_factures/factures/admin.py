fc55d6870e69a8ad99c204db4f46a49737afd5b8
Ambre-guia <ambreguia77@gmail.com>
2024-10-23 10:28:30 +0200

    exo 2 update

--- gestion_factures/factures/admin.py
+++ gestion_factures/factures/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Facture
 
-# Register your models here.
+def marquer_comme_paye(modeladmin, request, queryset):
+    queryset.update(est_paye=True)
+    
+marquer_comme_paye.short_description = "Marquer les factures comme payées"
+
+class FactureAdmin(admin.ModelAdmin):
+    list_display = ('titre', 'client', 'montant', 'date_emission', 'est_paye')
+    list_filter = ('client', 'est_paye')    
+    search_fields = ('client__nom', 'titre')
+    actions = [marquer_comme_paye]
+
+admin.site.register(Facture, FactureAdmin)
